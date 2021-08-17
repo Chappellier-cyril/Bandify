6643570712b936ef6fed45a2d8fb27b8297e2ba9
amaury.delaroque <amaury.delaroque@gmail.com>
2021-08-17 22:33:49 +0200

    logic around instrument/level input in Signup component

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -5,8 +5,9 @@ import levelsData from 'src/data/levels';
 
 /*
 Avec Redux :
- Prévoir les appels à la BDD pour vérifiez si l'email existe déjà l'email
+ Prévoir les appels à la BDD pour vérifiez si l'email existe déjà
  Vérifiez si l'utilisateur à plus de 15 ans
+
 */
 
 const Signup = () => {
@@ -17,9 +18,8 @@ const Signup = () => {
   const [description, setDescription] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [selectedInstrument, setSelectedInstrument] = useState('');
   // le state instrument sera un tableau qui récupère l'instrument et le niveau dans un objet
-  const [instruments, setInstruments] = useState([]);
+  const [instruments, setInstruments] = useState([{}]);
 
   /* fonction générique pour changer les inputs afin d'avoir des composants contrôlées
     @params setState => fonction du useState de l'input contrôlé
@@ -28,15 +28,33 @@ const Signup = () => {
   const onChangeInput = (setState, event) => {
     setState(event.target.value);
   };
-  // fonction qui associe l'instrument séléctionné avec le level dans
-  // un objet du tableau de state instruments
-  const onSelectLevelSetInstruments = (e) => {
-    if (selectedInstrument) {
-      setInstruments([...instruments, {
-        instrument: selectedInstrument,
+  // fonction pour copié l'instrument séléctionné
+  const onSelectInput = (e, index, key) => {
+    const copyInstrument = [...instruments];
+    copyInstrument[index] = {
+      ...copyInstrument[index][key],
+      instrument: e.target.value,
+    };
+    setInstruments(copyInstrument);
+    console.log(copyInstrument);
+  };
+  const onSelectLevel = (e, index) => {
+    const copyInstrument = [...instruments];
+    if (copyInstrument[index].instrument) {
+      copyInstrument[index] = {
+        ...copyInstrument[index],
         level: e.target.value,
-      }]);
+      };
+      setInstruments(copyInstrument);
+      console.log(copyInstrument);
     }
+  };
+  const addNewInputInstrument = () => {
+    setInstruments([...instruments, {}]);
+  };
+  const removeInputInstrument = (index) => {
+    const copyInstruments = instruments.filter((inst, i) => i !== index);
+    setInstruments(copyInstruments);
   };
   return (
     <form>
@@ -82,20 +100,40 @@ const Signup = () => {
           <input name="avatar" id="avatar" type="file" placeholder="Choisir une photo" />
         </label>
       </div>
-      <div>
-        <select name="instrument" id="instrument" onChange={(e) => onChangeInput(setSelectedInstrument, e)} required>
-          <option value="">Choisir un instrument</option>
-          {
-            instrumentsData.map(({ name, id }) => <option value={id} key={id}>{name}</option>)
-          }
-        </select>
-        <select name="level" id="level" onChange={onSelectLevelSetInstruments}>
-          <option value="">Choisir un niveau de pratique</option>
-          {
-            levelsData.map(({ name, id }) => <option value={id} key={id}>{name}</option>)
-          }
-        </select>
-      </div>
+
+      {
+        // on boucle sur le tableau d'instruments
+        instruments.map((instrument, index) => (
+          // Prévoir de générer un id pour un code plus propre
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={index}>
+            <select name={`instrument${index}`} id={`instrument${index}`} onChange={(e) => onSelectInput(e, index, 'instrument')} required>
+              <option value="">Choisir un instrument</option>
+              {
+                instrumentsData.map(({ name, id }) => <option value={id} key={id}>{name}</option>)
+              }
+            </select>
+            <select name={`level${index}`} id={`level${index}`} onChange={(e) => onSelectInput(e, index, 'level')} disabled={!instrument.instrument}>
+              <option value="">Choisir un niveau de pratique</option>
+              {
+                levelsData.map(({ name, id }) => <option value={id} key={id}>{name}</option>)
+              }
+            </select>
+            {
+              // Ici on disabled le bouton + si pas d'instrument choisi
+              // On ajoute un bouton  - à la ligne précédente si ajoute une ligne
+              // On limite le nombre de choix max du membre
+              // A voir combien d'instruments maximum on pourrais choisir
+              index < 3
+                && (index === instruments.length - 1
+                  ? <button type="button" onClick={addNewInputInstrument} disabled={!instrument.instrument}>+</button>
+                  : <button type="button" onClick={() => removeInputInstrument(index)}>-</button>
+                )
+            }
+          </div>
+        ))
+      }
+
     </form>
   );
 };
