88f99f69a585bfa37da9e4715d03d410dcb483e7
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2021-08-19 10:42:43 +0200

    userMiddleWare link Front & Back

--- src/middlewares/usersMiddleware.js
+++ src/middlewares/usersMiddleware.js
@@ -1,8 +1,20 @@
-// const usersMiddleware = (store) => (next) => async (action) => {
-//   // if (action.type === 'ON_SEARCH_SUBMIT') {
-//   //   // TODO
-//   // }
-//   next();
-// };
+import axios from 'axios';
 
-// export default usersMiddleware;
+const authMiddleware = (store) => (next) => (action) => {
+  // if (action.type === 'ON_LOGIN_SUBMIT') {
+  //   const state = store.getState();
+  //   console.log('logged !');
+  //   store.dispatch({ type: 'ON_LOGIN_SUCCESS' });
+  // }
+  const options = {
+    method: 'GET',
+    url: 'http://localhost:3000/members',
+  };
+
+  axios(options).then((response) => {
+    console.log(response.data);
+  });
+  next(action);
+};
+
+export default authMiddleware;
