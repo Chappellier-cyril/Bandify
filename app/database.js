dd9a7fabbed123753d2e051ec93bad115b0dd078
chappellier-cyril <cyril.chappellier@gmail.com>
2021-08-23 18:34:14 +0200

    add ssl true

--- app/database.js
+++ app/database.js
@@ -3,6 +3,9 @@ const { Sequelize } = require('sequelize');
 // 2. Créer une instance de sequelize
 sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+}
 
 });
 
