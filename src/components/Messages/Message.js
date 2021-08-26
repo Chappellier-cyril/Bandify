29df1658684824656388b433f646b8fb967d94d6
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2021-08-26 14:06:03 +0200

    components chatForm & Messages/Message + style

---
+++ src/components/Messages/Message.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Message = () => (
+  <li className="message">
+    <p className="message__author">Yo ça va?</p>
+    <p className="message__content">Bien et toi ?</p>
+  </li>
+);
+
+export default Message;
+
+
+
