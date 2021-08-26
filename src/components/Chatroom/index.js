3eb4f910c5481fb9a6b0de8c2dda879b62a88861
baptaste <babachampbenoit@gmail.com>
2021-08-26 14:06:57 +0200

    chatroom nav added

--- src/components/Chatroom/index.js
+++ src/components/Chatroom/index.js
@@ -4,10 +4,31 @@ import './style.scss';
 
 const Chatroom = ({ isChatroomOpen, toggleIsChatroomOpen }) => (
   <div className={isChatroomOpen ? 'chatroom chatroom--open' : 'chatroom'}>
-    Comopsannt chatroom
+    <div className="chatroom__nav">
+      <button type="button" className="chatroom__nav-link">Conversation</button>
+      <button type="button" className="chatroom__nav-link">Amis</button>
+    </div>
+
     <button type="button" onClick={toggleIsChatroomOpen} className="close-menu-btn">
       <i className="fas fa-times" />
     </button>
+
+    <div className="chatroom__messages">
+      <p className="chatroom__messages-item">Salut</p>
+      <p className="chatroom__messages-item">Ca va ?</p>
+      <p className="chatroom__messages-item">Non</p>
+      <p className="chatroom__messages-item">Ma connexion est a chier.</p>
+    </div>
+
+    <div className="chatroom__friends">
+      <ul className="chatroom__friends-list">
+        <li className="chatroom__friends-list__member">Roger</li>
+        <li className="chatroom__friends-list__member">Bardolph</li>
+        <li className="chatroom__friends-list__member">Tim</li>
+        <li className="chatroom__friends-list__member">Jason</li>
+      </ul>
+    </div>
+
   </div>
 );
 
