935de1c5e80687c3dc769ebc09ff05af2276ed59
chappellier-cyril <cyril.chappellier@gmail.com>
2021-08-30 17:30:56 +0200

    modify .env

--- s3/index.js
+++ s3/index.js
@@ -1,9 +1,9 @@
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
+const bucketName = process.env.S3_BUCKET_NAME;
+const region = process.env.AWS_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new S3({
     region,
