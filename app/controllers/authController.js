fb25f6304a09fe55ea40e972eebbe68acb0442bb
chappellier-cyril <cyril.chappellier@gmail.com>
2021-09-01 14:48:53 +0200

    added checkpassword method

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,46 +1,37 @@
 const {Member} = require('../models')
+const bcrypt = require('bcrypt')
 
 const authController = {
 
 checkPassword : async (req, res) => {
 
-    const targetId = req.params.id;
-    try {
+try {
     
-      const password = await Member.findByPk({
-          where: {
-              id : targetId,
-              password: req.body.user_password
-          },
-      });
-      console.log(password)
-
-      if(!password) {
-        throw({error : 'Mot de passe incorrects'});
-      }
+    const targetId = req.params.id;
+    const member = await Member.findByPk(targetId);
   
-      const memberPassword = member.user_password;
+    const memberPassword = member.user_password;
 
-      const isPasswordValid = await bcrypt.compare(req.body.user_password, memberPassword);
+    const isPasswordValid = await bcrypt.compare(req.body.user_password, memberPassword);
 
       // Si le mot de passe n'est pas valide on passe dans le catch
-      if(!isPasswordValid) {
-        throw({error : 'Mot de passe incorrects!'});
-      }
+    if(!isPasswordValid) {
+        res.json({error : 'Mot de passe incorrects!'});
+    }else {
 
-      // JWT Config
-      const jwtSecret = process.env.TOKEN_SECRET;
-      const jwtContent = { memberId: member.id };
-      const jwtOptions = { 
+           // JWT Config
+    const jwtSecret = process.env.TOKEN_SECRET;
+    const jwtContent = { memberId: member.id };
+    const jwtOptions = { 
       algorithm: 'HS256', 
       expiresIn: '3h' 
-    };
-      // Envoi de la réponse au front si tout est ok
-      
+        };
+          
     res.json({message: "Succes"});
+    }
 
-    } catch(error) {
-        res.json({error: "t'es pas au bonne endroit boby"})
+} catch(error) {
+        res.status(500).json(error)
     }
 },
 }
