51912fdcbe561b2a30cf5a581f62925b3ea09cd6
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2021-09-01 12:20:59 +0200

    Scss contact Page Mobile ok

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,33 +5,55 @@ import './style.scss';
 const Contact = () => (
   // TODO champs contrôlés, récup du message + page
   // TODO "votre message a bien été renvoyer" + redirect home
-  <div className="contact-submit__container">
-    <h1 className="contact-submit__title">Nous contacter</h1>
+    <form type="submit" className="contact__form">
+      <h1 className="contact__form--title">Nous contacter</h1>
 
-    <form type="submit" className="contact-submit__form">
-      <div className="contact-submit__group">
-        <label htmlFor="firstName">
-          <input name="firstName" id="firstName" type="text" placeholder="Prénom" required />
-        </label>
+      <div className="contact__form__container">
+        <input
+          className="contact__form__container--input"
+          name="firstName"
+          id="firstName"
+          type="text"
+          placeholder="Prénom"
+          required
+        />
       </div>
-      <div className="contact-submit__group">
-        <label htmlFor="lastName">
-          <input name="lastName" id="lastName" type="text" placeholder="Nom" required />
-        </label>
+      <div className="contact__form__container">
+        <input
+          className="contact__form__container--input"
+          name="lastName"
+          id="lastName"
+          type="text"
+          placeholder="Nom"
+          required
+        />
       </div>
-      <div className="contact-submit__group">
-        <label htmlFor="email">
-          <input name="email" id="email" type="email" placeholder="Email" required />
-        </label>
+      <div className="contact__form__container">
+        <input
+          className="contact__form__container--input"
+          name="email"
+          id="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
       </div>
-      <div className="contact-submit__group__message">
-        <label htmlFor="message">
-          <textarea name="description" id="message" type="text" placeholder="Message" required />
-        </label>
+      <div className="contact__form__container__message">
+        <textarea
+          className="contact__form__container__message--input"
+          name="description"
+          id="message" type="text"
+          placeholder="Message"
+          required
+        />
       </div>
-      <button type="submit">ENVOYER</button>
+      <button
+        type="submit"
+        className="contact__form--submit"
+      >
+        Envoyer
+      </button>
     </form>
-  </div>
 );
 
 export default Contact;
