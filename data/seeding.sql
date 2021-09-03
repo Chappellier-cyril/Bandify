76c666c7c41da0e7e0cd4693daff8d64dbbc4d77
chappellier-cyril <cyril.chappellier@gmail.com>
2021-09-03 17:53:56 +0200

    add Readme.md

--- data/seeding.sql
+++ data/seeding.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO instrument ("instrument_name")VALUES
+INSERT INTO "instrument" ("instrument_name")VALUES
 ('Guitare'),
 ('Basse'),
 ('Piano'),
@@ -14,7 +14,6 @@ INSERT INTO instrument ("instrument_name")VALUES
 ('Batterie'),
 ('Saxophone'),
 ('Trompette'),
-('Tuba'),
 ('Djembé'),
 ('Maracas'),
 ('Accordéon'),
@@ -53,7 +52,7 @@ INSERT INTO instrument ("instrument_name")VALUES
 ('Vibraphone'),
 ('Chant');
 
-INSERT INTO music_style ("music_name") VALUES
+INSERT INTO "music_style" ("music_name") VALUES
 ('Rock'),
 ('Jazz'),
 ('Electro'),
@@ -103,7 +102,7 @@ INSERT INTO level ("level_name") VALUES
 ('Intermédiaire'),
 ('Avancé');
 
-INSERT INTO member ("firstname", "lastname", "email", "birthdate" , "user_password", "user_description", "profil_image", "city_code") VALUES
+INSERT INTO "member" ("firstname", "lastname", "email", "birthdate" , "user_password", "user_description", "profil_image", "city_code") VALUES
 ('Cyril', 'Chappellier', 'cyril-c@gmail.com','1990-01-09' , '$2b$10$79fDqa0fB.yd2H0eB8c5b./Mr.IuefSR9nvKFP0PP0tFkGJl4Rs6i', 'Je recherche un groupe de metal pour faire de la guitare éléctrique', 'cyril.png', '75056'),
 ('Jeremy', 'Marques', 'jeremy-m@gmail.com','1989-12-12' , '$2b$10$79fDqa0fB.yd2H0eB8c5b./Mr.IuefSR9nvKFP0PP0tFkGJl4Rs6i', 'Guitariste confirmé, je recherche un batteur et un bassiste, pour monter un groupe de musique alternative', 'jeremy.png', '45234'),
 ('Arnaud', 'Ferreira', 'arnaud-m@gmail.com','1992-08-31' , '$2b$10$79fDqa0fB.yd2H0eB8c5b./Mr.IuefSR9nvKFP0PP0tFkGJl4Rs6i', 'Pianiste amateur, je cherche un groupe de salsa', 'arnaud.png', '77288'),
@@ -111,19 +110,19 @@ INSERT INTO member ("firstname", "lastname", "email", "birthdate" , "user_passwo
 ('Amaury', 'Delaroque', 'amaury-d@gmail.com','1986-02-10' , '$2b$10$79fDqa0fB.yd2H0eB8c5b./Mr.IuefSR9nvKFP0PP0tFkGJl4Rs6i', 'Dj et guitariste, je cherche des samples pour composition de musique hip-hop', 'amaury.png', '31555');
 
 
-INSERT INTO invitation ("status", "from", "to") VALUES
+INSERT INTO "invitation" ("status", "from", "to") VALUES
 (2, 3, 2),
 (1, 1, 2),
 (0, 4, 1);
 
-INSERT INTO user_has_instrument_level ("instrument_id", "level_id", "member_id") VALUES
+INSERT INTO "user_has_instrument_level" ("instrument_id", "level_id", "member_id") VALUES
 (1, 3, 1),
 (3, 1, 2),
 (1, 1, 3),
 (1, 1, 4),
 (1, 1, 5);
 
-INSERT INTO appreciate_music_style ("member_id", "music_style_id") VALUES
+INSERT INTO "appreciate_music_style" ("member_id", "music_style_id") VALUES
 (1, 1),
 (2, 4),
 (3, 9),
