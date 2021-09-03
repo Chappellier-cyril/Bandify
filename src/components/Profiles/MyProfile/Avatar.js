4576caed8c4a6893b315c5b8b44097450f505001
amaury.delaroque <amaury.delaroque@gmail.com>
2021-09-03 11:16:11 +0200

    validators on change avatar or add a sound

--- src/components/Profiles/MyProfile/Avatar.js
+++ src/components/Profiles/MyProfile/Avatar.js
@@ -1,11 +1,23 @@
 /* eslint-disable camelcase */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const Avatar = ({
   editPhoto, handleSubmitPhoto, profil_image, editFormToggle,
 }) => {
   const [avatar, setAvatar] = useState();
+  const [errorAvatar, setErrorAvatar] = useState('');
+  useEffect(() => {
+    if (avatar) {
+      const allowedExtension = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+      if (!allowedExtension.exec(avatar.name)) {
+        return setErrorAvatar('Le fichier que vous avez séléctionné n\'a pas le format autorisé. Veuillez choisir un fichier image au format *.jpeg / *.png / *.jpg');
+      }
+      if (avatar.size > 2000000) return setErrorAvatar('Le fichier que vous séléctionné est trop volumineux. Veuillez choisir un fichier de taille 2mo maximum');
+      return setErrorAvatar('');
+    }
+    return null;
+  }, [avatar]);
   return (
     <>
       {editPhoto ? (
@@ -16,16 +28,22 @@ const Avatar = ({
         >
           <div className="myprofile__user--picture-desc">
             <p>Image de profil</p>
-            <label htmlFor="avatar" className="myprofile__user--picture-file">
+            <label htmlFor="avatar" className="signup-submit__group--avatar__container">
+              <span className="signup-submit__group--avatar__container__label">Choisir une photo</span>
               <input
                 name="avatar"
                 id="avatar"
                 type="file"
                 placeholder="Choisir une photo"
+                className="signup-submit__group__input--avatar"
                 onChange={(e) => setAvatar(e.target.files[0])}
               />
+              {errorAvatar && <p className="signup-submit__error">{errorAvatar}</p>}
+              <div className="signup-submit__container-shown-avatar">
+                {avatar && <img className="signup-submit__show-avatar" src={URL.createObjectURL(avatar)} alt={`Votre fichier séléctionné est ${avatar.name}`} />}
+              </div>
             </label>
-            <button type="submit">Envoyer</button>
+            <button type="submit" disabled={errorAvatar}>Envoyer</button>
           </div>
           <button
             type="button"
