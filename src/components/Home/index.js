5774515f5226edcefaaf29e6186c92318fbf765e
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2021-09-05 16:46:15 +0200

    bug fix on my profil width

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -37,7 +37,7 @@ const Home = ({
     centerMode: true,
     responsive: [
       {
-        breakpoint: 1400,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
