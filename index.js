f8026f0e653e29c83454c48f06694c8c9aa209a0
chappellier-cyril <cyril.chappellier@gmail.com>
2021-09-07 12:26:17 +0200

    modify clientside and cors

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const io = socketio(server, {
 app.use(express.json());
 app.use(express.static('upload'));
 
-app.use( cors('https://bandify.fr') );
+app.use( cors('https://www.bandify.fr') );
 
 app.use(express.urlencoded({extended: true})); 
 
